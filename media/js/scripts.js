4d4fa1950120cbcd4bf5495d9d0648a931709ac7
Jakub Rodziński <jakub.rodzinski@gmail.com>
2015-06-03 01:15:15 +0200

    form validation

--- media/js/scripts.js
+++ media/js/scripts.js
@@ -1,121 +1,304 @@
 $(function() {
 
-	// ustawienie wysokości
-	$(".content").height($(window).height() - $("header").height());
-	// nie ustawiaj wysokości dla drugiego panela dla telefonu
+// ================= USTAWIENIE STAŁEJ WYSOKOŚCI SEKCJI =================
+
+	// jeżeli smartfon (pxrt >= 2), to nie ustawiaj stałej wysokości drugiej sekcji
+
 	if(window.devicePixelRatio < 2) {
-		$(".second-panel, .third-panel").height($(window).height());
+
+		$(".content").height( $(window).height() - $("header").height() );
+		$(".second-panel, .third-panel").height( $(window).height() );
+
 	} else {
-		$(".third-panel").height($(window).height());
+
+		$(".third-panel").height( $(window).height() );
+
 	}
 
-	var handleResize = function(e) {
-		e.preventDefault();
-		$(".second-panel, .third-panel").height($(this).height());
-		$(".content").height($(this).height() - $("header").height());
-	};
+// =============================== TOOLS ================================
 
-	var handleArrow = function(e) {
+	var tools = {
 
-		// jeżeli mobilne to nie używaj scrollify, po prostu przelinkuj
-		if(window.devicePixelRatio === 1) {
-			e.preventDefault();
-			var link = $(this).attr("href");
-			$.scrollify("move", link);
-		} else {
-			var firstPanel = $(".first-panel");
-			var link = firstPanel.data("section-name");
-			firstPanel.attr("id", link);
+		// flexbox z przedrostkami
+
+		setDisplayFlex: function(el) {
+
+			el.css({
+				display: "-webkit-flex",
+				display: "-moz-flex",
+				display: "-ms-flex",
+				display: "-o-flex",
+				display: "flex"
+			});
+
 		}
 
-		// Btn UTWÓRZ KONTO na ostatniej stronie
-		// ustaw panel rejestracji, jeśli był wcześniej wybrany panel logowania
-		if(link == "#first" || link == "first") {
+	};
 
-			if($(".login-form").css("display") !== "none") {
 
-				var rightBox = $(".right-box");
-				rightBox.children("h2").html("<span>Zarejestruj się</span> już teraz:");
-				rightBox.children("p").html("Masz już konto? <a class='sign-in-link'>Zaloguj się</a>");
+// ==================== ZMIANA ROZMIARU PRZEGLĄDARKI ====================
 
-				$(".login-form").css("display", "none");
+	var handleResize = {
 
-				$(".registry-form").css({
-					display: "-webkit-flex",
-					display: "-moz-flex",
-					display: "-ms-flex",
-					display: "-o-flex",
-					display: "flex"
-				});
+		// automatycznie dostosuj wysokość sekcji podczas zmiany rozmiaru okna
 
-			}
+		init: function(e) {
+
+			var viewport = e.target;
+
+			$(".second-panel, .third-panel").height( $(viewport).height() );
+			$(".content").height( $(viewport).height() - $("header").height() );
+
+		}
+
+	};
+
+// =============================== ARROW ================================
+
+	var handleArrow = {
+
+		setLink: function() {
+
+			this.link = "";
+
 			if(window.devicePixelRatio === 1) {
-				$(".email-reg-input").focus();
+
+				this.link = $(this.arrow).attr("href");
+				$.scrollify("move", this.link);
+
+			} else {
+
+				var firstPanel = $(".first-panel");
+				this.link = firstPanel.data("section-name");
+				firstPanel.attr("id", this.link);
+
 			}
+
+		},
+
+		setForm: function() {
+
+			if(this.link === "#first" || this.link === "first") {
+
+				if( $(".login-form").css("display") !== "none" ) {
+
+					var rightBox = $(".right-box");
+					rightBox.children("h2").html("<span>Zarejestruj się</span> już teraz:");
+					rightBox.children("p").html("Masz już konto? <a class='sign-in-link'>Zaloguj się</a>");
+
+					$(".login-form").css("display", "none");
+
+					tools.setDisplayFlex($(".registry-form"));
+
+				}
+
+			}
+
+		},
+
+		mouseIn: function() {
+
+			$(this.arrow).css({
+				"-webkit-animation": "arrow-anim2 2s infinite",
+				"-moz-animation": "arrow-anim2 2s infinite",
+				"-ms-animation": "arrow-anim2 2s infinite",
+				"-o-animation": "arrow-anim2 2s infinite",
+				animation: "arrow-anim2 2s infinite"
+			});
+
+		},
+
+		mouseOut: function() {
+
+			$(this.arrow).css({
+				"-webkit-animation": "arrow-anim 2s infinite",
+				"-moz-animation": "arrow-anim 2s infinite",
+				"-ms-animation": "arrow-anim 2s infinite",
+				"-o-animation": "arrow-anim 2s infinite",
+				animation: "arrow-anim 2s infinite"
+			});
+
+		},
+
+		init: function(e) {
+
+			this.arrow = e.currentTarget;
+			var event = e.type;
+
+			if(event === "click") {
+
+				// ustaw href, oraz wykorzystaj scrollify tylko na komputerze
+
+				this.setLink();
+
+				// jeżeli przełączono na formularz logowania, ustaw domyślnie formularz rejestracji
+
+				this.setForm();
+
+			// zmiana animacji
+
+			} else if(event === "mouseenter") {
+
+				this.mouseIn();
+
+			} else if (event === "mouseleave") {
+
+				this.mouseOut();
+
+			}
+
 		}
+
 	};
 
-	var mouseIn = function(e) {
-		e.preventDefault();
-		$(this).css({
-			"-webkit-animation": "arrow-anim2 2s infinite",
-			"-moz-animation": "arrow-anim2 2s infinite",
-			"-ms-animation": "arrow-anim2 2s infinite",
-			"-o-animation": "arrow-anim2 2s infinite",
-			animation: "arrow-anim2 2s infinite"
-		});
-	};
+// ==================== OBSŁUGA LINKU FORMULARZA ====================
 
-	var mouseOut = function(e) {
-		e.preventDefault();
-		$(this).css({
-			"-webkit-animation": "arrow-anim 2s infinite",
-			"-moz-animation": "arrow-anim 2s infinite",
-			"-ms-animation": "arrow-anim 2s infinite",
-			"-o-animation": "arrow-anim 2s infinite",
-			animation: "arrow-anim 2s infinite"
-		});
-	};
+	var handleFormLink = {
 
-	var handleFormLink = function(e) {
-		e.preventDefault();
+		setFormData: function() {
 
-		var rightBox = $(".right-box");
-		rightBox.hide();
+			if(this.link.className == "sign-in-link") {
 
-		var showForm = {};
-		var hideForm = {};
-		// sprawdź, który panel aktualnie jest widoczny
-		if(this.className == "sign-in-link") {
-			showForm = $(".login-form");
-			hideForm = $(".registry-form");
-			rightBox.children("h2").html("<span>Zaloguj się</span>:");
-			rightBox.children("p").html("Nie masz jeszcze konta? <a class='sign-up-link'>Zarejestuj się</a>");
-		} else {
-			showForm = $(".registry-form");
-			hideForm = $(".login-form");
-			rightBox.children("h2").html("<span>Zarejestruj się</span> już teraz:");
-			rightBox.children("p").html("Masz już konto? <a class='sign-in-link'>Zaloguj się</a>");
+				this.hiddenForm = $(".login-form");
+				this.visibleForm = $(".registry-form");
+				this.rightBox.children("h2").html("<span>Zaloguj się</span>:");
+				this.rightBox.children("p").html("Nie masz jeszcze konta? <a class='sign-up-link'>Zarejestuj się</a>");
+
+			} else {
+
+				this.hiddenForm = $(".registry-form");
+				this.visibleForm = $(".login-form");
+				this.rightBox.children("h2").html("<span>Zarejestruj się</span> już teraz:");
+				this.rightBox.children("p").html("Masz już konto? <a class='sign-in-link'>Zaloguj się</a>");
+
+			}
+
+		},
+
+		showForm: function() {
+
+			this.visibleForm.css("display", "none");
+
+			tools.setDisplayFlex(this.hiddenForm);
+
+		},
+
+		init: function(e) {
+
+			this.link = e.currentTarget;
+			this.rightBox = $(".right-box");
+
+			// ukryj cały prawy box
+
+			this.rightBox.hide();
+
+			// ustaw odpowiedni nagłówek oraz odnośnik do innego formularza
+
+			this.setFormData();
+
+			// ukryj/pokaż odpowiednie formularze
+
+			this.showForm();
+
+			// pokaż cały prawy box
+
+			this.rightBox.fadeIn(300);
+
+			// ustaw focus na emailu, jeżeli komputer
+
+			if(window.devicePixelRatio === 1) {
+				this.hiddenForm.children("input:first-child").focus();
+			}
+
 		}
 
-		hideForm.css("display", "none");
-
-		showForm.css({
-			display: "-webkit-flex",
-			display: "-moz-flex",
-			display: "-ms-flex",
-			display: "-o-flex",
-			display: "flex"
-		});
-
-		rightBox.fadeIn(300);
-
-		if(window.devicePixelRatio === 1) {
-			showForm.children("input:first-child").focus();
-		}
 	};
+
+// ========================= WALIDACJA FORMULARZY =========================
+
+	var handleFormSubmit = {
+
+		setStyles: function(el, msg) {
+
+			// this: obiekt
+			$(el).css("border", "2px solid #A60404");
+			$('.' + el.className + ' + .warning').css("display", "block").text(msg);
+			this.e.preventDefault();
+
+		},
+
+		checkEmpty: function(el) {
+
+			// this: obiekt
+			var errorMsg = "Pole nie może być puste.";
+
+			if(el.value === "") {
+				this.setStyles(el, errorMsg);
+				return false;
+			}
+
+			return true;
+
+		},
+
+		checkValid: function(elements) {
+
+			var emailRegEx = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+			var errorMsg = "";
+			var self = this;
+
+			// this: konkretny input
+			// self: obiekt
+			$(elements).each(function(){
+
+				if(self.checkEmpty(this) === false) {
+					return;
+				}
+
+				if( this.name === "email" && !emailRegEx.test($(this).val()) ) {
+
+					errorMsg = "Podano niepoprawny format adresu e-mail.";
+					self.setStyles(this, errorMsg);
+
+				} else if( this.name === "password" && $(this).val().length < 8 ) {
+
+					errorMsg = "Hasło musi mieć minimum 8 znaków.";
+					self.setStyles(this, errorMsg);
+
+				} else if( this.name === "confirm" && $(this).val() !== $("input[name='password']").val() ) {
+
+					errorMsg = "Hasła się nie zgadzają.";
+					self.setStyles(this, errorMsg);
+
+				}
+
+			});
+
+		},
+
+		init: function(e) {
+
+			this.form = e.target;
+			this.e = e;
+
+			// wyzerowanie ostrzeżeń oraz obramowania inputów
+
+			$(".warning").text("");
+			$("input").not("input[type='submit']").css("border", "none");
+
+			// waliduj pola formularza
+
+			this.checkValid(this.form.elements);
+
+		}
+
+	};
+
+// ========================= OBSŁUGA ZDARZEŃ =========================
+
 
 	// uruchom scrollify na komputerze
+
 	if(window.devicePixelRatio === 1) {
 
 		$.scrollify({
@@ -128,19 +311,51 @@ $(function() {
 			after:function() {}
 	    });
 
+        // wyłączenie scrollify gdy focus na inpucie
+
+	    $("input").on('keyup', function(e) {
+	    	e.stopPropagation();
+	    });
+
 	}
 
+
 	// zdarzenie zmiany rozmiarów przeglądarki
+	// tylko na komputerze
+
 	if(window.devicePixelRatio === 1) {
-		$(window).on("resize", handleResize);
+
+		$(window).on("resize", function(e) {
+    		handleResize.init(e);
+    	});
+
 	}
-	// zdarzenia kliknięcia i hovera strzałki
-	if($(window).width() > 900) {
-		$(".arrow").on("click", handleArrow);
-		$(".arrow").hover(mouseIn, mouseOut);
-	}
-	// zdarzenia kliknięcia na UTWÓRZ KONTO z ostatniego panelu
-    $(".back-registry").on("click", handleArrow);
+
+
+	// zdarzenie kliknięcia i hovera strzałki
+	// oraz kliknięcia buttona w ostatniej sekcji
+
+	$(".arrow").on("click mouseenter mouseleave", function(e) {
+		handleArrow.init(e);
+	});
+	$(".back-registry").on("click", function(e) {
+		handleArrow.init(e);
+	});
+
+
     // zdarzenie kliknięcia na link Zaloguj się lub Zarejestruj się
-    $(".right-box").on("click", "p > a", handleFormLink);
-})
+
+    $(".right-box").on("click", "p > a", function(e) {
+		handleFormLink.init(e);
+	});
+
+
+    // walidacja formularzy
+
+    $(".registry-form, .login-form").on("submit", function(e) {
+    	handleFormSubmit.init(e);
+    });
+
+// ========================= OBSŁUGA ZDARZEŃ =========================
+
+});
